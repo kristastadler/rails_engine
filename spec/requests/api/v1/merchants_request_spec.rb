af963f250b9292eb0a17a4082bd614e143156e57
“Krista <stadler.krista@gmail..com>
2020-05-18 20:22:16 -0600

    Test for index written

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of merchants" do
+    create_list(:merchant, 4)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(4)
+  end
+
+  
+end
