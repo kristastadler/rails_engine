8fa3eb077de652f947d7c20d73f7719d772cd151
“Krista <stadler.krista@gmail..com>
2020-05-19 14:34:45 -0600

    Single finder for items and merchants is functional and tested

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,19 +1,16 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    merchant = matches.first
+    @merchants = Merchant.where(nil)
+    filtering_params(params).each do |key, value|
+      @merchants = @merchants.public_send("filter_by_#{key}", value) if value.present?
+    end
+    merchant = @merchants.first
     render json: MerchantSerializer.new(merchant)
   end
 
   private
 
-  def matches
-    search_params.to_h.inject([]) do |merchants, (attr, value)|
-      merchants << Merchant.where("lower(#{attr}) LIKE ?", "%#{value}")
-      merchants.flatten
-    end
-  end
-
-  def search_params
-    params.permit(:name, :created_at, :updated_at)
+  def filtering_params(params)
+    params.slice(:name, :created_at, :updated_at)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
+  scope :filter_by_name, -> (name) { where "lower(name) LIKE ?", "%#{name}%" }
+  scope :filter_by_created_at, -> (created_at) { where created_at: created_at }
+  scope :filter_by_updated_at, -> (updated_at) { where updated_at: updated_at }
 end
