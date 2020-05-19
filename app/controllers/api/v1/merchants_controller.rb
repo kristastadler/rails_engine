a1ae515a4a88003900cc4b91f7f20924232d2311
“Krista <stadler.krista@gmail..com>
2020-05-18 20:35:02 -0600

    Finished update endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,31 +3,31 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.all)
   end
 
-  # def show
-  #   render json: ItemSerializer.new(Item.find(params[:id]))
-  # end
-  #
-  # def create
-  #   item = Item.create!(item_params)
-  #   render json: ItemSerializer.new(item)
-  # end
-  #
-  # def update
-  #   item = Item.find(params[:id])
-  #   item.update(item_params)
-  #   render json: ItemSerializer.new(item)
-  # end
-  #
+   def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  def create
+    merchant = Merchant.create!(merchant_params)
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    render json: MerchantSerializer.new(merchant)
+  end
+
   # def destroy
   #   item = Item.find(params[:id])
   #   render json: ItemSerializer.new(item)
   #   Item.delete(params[:id])
   # end
-  #
-  # private
-  #
-  # def merchant_params
-  #   params.permit(:name)
-  # end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -13,5 +13,36 @@ describe "Merchants API" do
     expect(merchants["data"].count).to eq(4)
   end
 
-  
+  it "can get one merchant by its id" do
+    create(:merchant, name: "Bob Evans")
+    id = Merchant.last.id
+    name = Merchant.last.name
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(merchant["data"]["attributes"]["name"]).to eq(name)
+  end
+
+  it "can create a new merchant" do
+    post "/api/v1/merchants", params: { name: "Johnny Appleseed" }
+    merchant = Merchant.last
+    parse_merchant = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(merchant.name).to eq("Johnny Appleseed")
+    expect(parse_merchant["data"]["attributes"]["name"]).to eq(merchant.name)
+  end
+
+  it "can update an existing merchant" do
+    id = create(:merchant).id
+    previous_name = Merchant.last.name
+
+    put "/api/v1/merchants/#{id}", params: {name: "Better Merchant"}
+    merchant = Merchant.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(merchant.name).to_not eq(previous_name)
+    expect(merchant.name).to eq("Better Merchant")
+  end
 end
