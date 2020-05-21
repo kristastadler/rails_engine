a94b5326ecbaf1fae4bd4f07790a360f7325aa5c
“Krista <stadler.krista@gmail..com>
2020-05-20 21:33:28 -0600

    Hard coded json response to turn unit prices into floats

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,4 +2,8 @@ class ItemSerializer
   include FastJsonapi::ObjectSerializer
   set_type :item
   attributes :name, :description, :unit_price, :merchant_id
+
+  attribute :unit_price do |price|
+    price.unit_price.to_f
+  end
 end
